de25932acb3a3978e37c1c00ea2cb923d2e64943
Gigiphilac <claydiebear@gmail.com>
2023-03-17 17:12:14 +1100

    part a done?

--- src/MyTimetable.java
+++ src/MyTimetable.java
@@ -11,6 +11,8 @@ public class MyTimetable {
     Scanner sc = new Scanner(System.in);
 
     List<Course> coursesArray = new ArrayList<>();
+    List<Course> coursesEnrolled = new ArrayList<>();
+
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("H:mm");
 
     public static void main(String[] args) {
@@ -58,36 +60,159 @@ public class MyTimetable {
                 Please select:\s""");
     }
 
-//    private String getCourseChoice() {
-//        int i=0;
-//        System.out.println("""
-//                --------------------------------------------------------------------------------
-//                > Select from main menu
-//                --------------------------------------------------------------------------------""");
-//        for (Course c : coursesArray) {
-//            System.out.println(i + ") " + c.getName());
-//        }
-//        return getString("Please select: ");
-//    }
 
     private void enrol() {
         String keyword = getString("Enter a keyword to search: ");
 
-        if (searchForMatch(keyword)) {
-            System.out.println("Match");
+        if (checkMatch(keyword)) {
+            outputMatches(keyword);
         } else {
-            System.out.println("No match");
+            System.out.println("No courses were found matching that keyword...");
         }
     }
 
 
-    private void showEnrolments() {
+    private boolean checkMatch(String keyword) {
+        for (Course c : coursesArray) {
+            if (c.getName().toLowerCase().contains(keyword.toLowerCase())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    private void outputMatches(String keyword) {
+
+        List<Course> matches = new ArrayList<>();
+        for (Course c : coursesArray) {
+            if (c.getName().toLowerCase().contains(keyword.toLowerCase())) {
+                matches.add(c);
+            }
+        }
+
+        System.out.println("""
+                --------------------------------------------------------------------------------
+                > Select from matching list
+                --------------------------------------------------------------------------------""");
+
+        int response;
+        Course selectedCourse;
+        do {
+            response = getCourseChoice(matches);
+        } while (response<1||response>matches.size()+1);
+
+        try {
+            selectedCourse = matches.get(response-1);
+            if (!checkDuplicates(coursesEnrolled, selectedCourse)) {
+                System.out.println("You have enrolled in the course '" + selectedCourse.getName() + "'!");
+                coursesEnrolled.add(selectedCourse);
+            } else {
+                System.out.println("You are already enrolled in the course '" + selectedCourse.getName() + "'.");
+            }
+
+        } catch (IndexOutOfBoundsException ignored) {}
 
     }
 
 
-    private void withdraw() {
+    private <T> boolean checkDuplicates(List<T> array, T element) {
+        for (T t : array) {
+            if (t.equals(element)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
+
+    private int getCourseChoice(List<Course> array) {
+        int i=1;
+        for (Course c : array) {
+            System.out.println(i+") "+c.getName());
+            i++;
+        }
+        System.out.println(i+") Return to menu");
+
+        return getInteger("Please select: ");
+    }
+
+
+    private void showEnrolments() {
+        if (!coursesEnrolled.isEmpty()) {
+            int i=1;
+
+            System.out.println("""
+                    --------------------------------------------------------------------------------
+                    You have enrolled into the following course(s):
+                    --------------------------------------------------------------------------------""");
+
+            for (Course c : coursesEnrolled) {
+
+                String day = c.getLtlDay().substring(0,3);
+                LocalTime timeStart = c.getLtlTime();
+                LocalTime timeEnd = addDoubleToTime(timeStart, c.getDuration());
+
+                String dayAndTime = String.format("%s %s-%s", day, timeStart, timeEnd);
+
+                System.out.printf("%d) %-30s%-15s%-18s\n", i, c.getName(), c.getDelivery(), dayAndTime);
+                i++;
+            }
+
+        } else {
+            System.out.println("You are not enrolled in any classes.");
+        }
+    }
+
+
+    private LocalTime addDoubleToTime(LocalTime time, double value) {
+        int hours = (int) value;
+        int minutes = (int) ((value - hours) * 60);
+
+        return time.plusHours(hours).plusMinutes(minutes);
+    }
+
+
+    private void withdraw() {
+        if (!coursesEnrolled.isEmpty()) {
+
+            System.out.println("""
+                    --------------------------------------------------------------------------------
+                    Please choose a course to withdraw:
+                    --------------------------------------------------------------------------------""");
+
+            int response;
+            Course selectedCourse;
+            do {
+                response = getWithdrawalChoice(coursesEnrolled);
+            } while (response<1||response>coursesEnrolled.size()+1);
+
+            try {
+                selectedCourse = coursesEnrolled.get(response-1);
+                System.out.println("You have withdrawn from '" + selectedCourse.getName() + "'!");
+                coursesEnrolled.remove(selectedCourse);
+            } catch (IndexOutOfBoundsException ignored) {}
+        } else {
+            System.out.println("You must be enrolled in at least one class to withdraw.");
+        }
+    }
+
+
+    private int getWithdrawalChoice(List<Course> array) {
+        int i=1;
+        for (Course c : array) {
+            String day = c.getLtlDay().substring(0,3);
+            LocalTime timeStart = c.getLtlTime();
+            LocalTime timeEnd = addDoubleToTime(timeStart, c.getDuration());
+
+            String dayAndTime = String.format("%s %s-%s", day, timeStart, timeEnd);
+
+            System.out.printf("%d) %-30s%-15s%-18s\n", i, c.getName(), c.getDelivery(), dayAndTime);
+            i++;
+        }
+        System.out.println(i+") Return to menu");
+
+        return getInteger("Please select: ");
     }
 
 
@@ -118,16 +243,6 @@ public class MyTimetable {
     }
 
 
-    private boolean searchForMatch(String keyword) {
-        for (Course c : coursesArray) {
-            if (c.getName().toLowerCase().contains(keyword.toLowerCase())) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-
     public String getString(String prompt) {
         String value;  // stores user input
 
@@ -136,6 +251,27 @@ public class MyTimetable {
             System.out.print(prompt);
             value = sc.nextLine();
         } while (value.equals(""));
+
+        return value;  // return the expected value
+    }
+
+
+    public int getInteger(String prompt) {
+        int value = 0;  // initialises user input variable to 0
+        boolean proceed = true;  // keeps do-while running while true
+
+        // as long as user input is not an integer, do-while will repeat
+        do {
+            System.out.print(prompt);
+            // try parsing input as Integer, and catches any failure to do so
+            try {
+                value = Integer.parseInt(sc.nextLine());
+                proceed = false;  // desired value obtained, allow loop to end
+            } catch (NumberFormatException e) {  // non-int inputted
+                // change prompt to provide direction to user
+                prompt = "Invalid input! You must enter an integer value: ";
+            }
+        } while (proceed);
 
         return value;  // return the expected value
     }
